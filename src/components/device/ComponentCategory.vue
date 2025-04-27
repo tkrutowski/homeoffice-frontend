ee850b2fbb9a70810ca101e0a38f15ec351bc25c
Tomek <tkrutowski@gmail.com>
2025-04-27 18:50:31 +0200

    CHARM-90 Integrate @heroicons/vue for component icons in ComponentCategory and update package dependencies

--- src/components/device/ComponentCategory.vue
+++ src/components/device/ComponentCategory.vue
@@ -1,6 +1,24 @@
 <script setup lang="ts">
 import {computed, type PropType, ref} from "vue";
 import type {ComponentType, Computer} from "@/types/Computer.ts";
+import {
+  ComputerDesktopIcon,
+  CpuChipIcon,
+  CircleStackIcon,
+  TableCellsIcon,
+  SpeakerWaveIcon,
+  WindowIcon,
+  KeyIcon,
+  CursorArrowRippleIcon,
+  BoltIcon,
+  UsersIcon,
+  ServerIcon,
+  TvIcon as DisplayIcon,
+  Square3Stack3DIcon,
+  ArrowPathRoundedSquareIcon,
+  PhotoIcon,
+  RectangleGroupIcon
+} from '@heroicons/vue/24/solid'
 
 const props = defineProps({
   componentType: {
@@ -31,6 +49,25 @@ const calculateQuantity = computed(() => {
   return 0;
 })
 
+const getIcon = computed(() => {
+  switch (props.componentType.name) {
+    case 'PROCESSOR': return CpuChipIcon
+    case 'MOTHERBOARD': return ServerIcon
+    case 'COMPUTER_CASE': return ComputerDesktopIcon
+    case 'RAM': return Square3Stack3DIcon
+    case 'GRAPHICS_CARD': return PhotoIcon
+    case 'DISK': return CircleStackIcon
+    case 'POWER': return BoltIcon
+    case 'DISPLAY': return DisplayIcon
+    case 'KEYBOARD': return KeyIcon
+    case 'MOUSE': return CursorArrowRippleIcon
+    case 'SOUND_CARD': return SpeakerWaveIcon
+    case 'USB': return UsersIcon
+    case 'COOLER': return ArrowPathRoundedSquareIcon
+    default: return ComputerDesktopIcon
+  }
+})
+
 const view = () => {
   if (selected.value) {
     selected.value = false
@@ -44,17 +81,20 @@ const view = () => {
 
 <template>
   <div
-      class="hover:bg-surface-100 hover:dark:bg-surface-800 border border-primary rounded-lg p-4 mb-4 cursor-pointer"
+      class="hover:bg-surface-100 hover:dark:bg-surface-800 border border-primary rounded-lg p-4 mb-4 cursor-pointer flex justify-between items-center"
       :class="{
         'dark:bg-surface-700 bg-surface-200': selected,
       }"
       @click="view"
   >
-    <p class="pb-2">
-      Kategoria: <span class="text-xl font-semibold pl-2">{{ props.componentType.viewName }}</span>
-    </p>
-    <p class="pb-2">
-      Ilość: <span class="font-semibold pl-2">{{ calculateQuantity }}</span>
-    </p>
+    <div>
+      <p class="pb-2">
+        Kategoria: <span class="text-xl font-semibold pl-2">{{ props.componentType.viewName }}</span>
+      </p>
+      <p class="pb-2">
+        Ilość: <span class="font-semibold pl-2">{{ calculateQuantity }}</span>
+      </p>
+    </div>
+    <component :is="getIcon" class="h-12 w-12 text-primary" />
   </div>
 </template>
